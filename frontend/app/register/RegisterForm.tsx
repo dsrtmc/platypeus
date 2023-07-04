550e06ac6e0f3ec73cbc16e17aae28382c98a62b
dsrtmc <radoslaw.ordecki@protonmail.com>
2023-07-04 13:00:16 +0200

    Add password field to the register form

--- frontend/app/register/RegisterForm.tsx
+++ frontend/app/register/RegisterForm.tsx
@@ -7,12 +7,13 @@ import { RegisterDocument } from "@/graphql/generated/graphql";
 export default function RegisterForm() {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const [register] = useMutation(RegisterDocument);
 
   async function handleSubmit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
-    await register({ variables: { username, email } });
+    await register({ variables: { username, email, password } });
   }
 
   return (
@@ -20,6 +21,12 @@ export default function RegisterForm() {
       <h3>register</h3>
       <input type={"text"} placeholder={"username"} value={username} onChange={(e) => setUsername(e.target.value)} />
       <input type={"email"} placeholder={"email"} value={email} onChange={(e) => setEmail(e.target.value)} />
+      <input
+        type={"password"}
+        placeholder={"password"}
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
       <button type={"submit"}>register</button>
     </form>
   );
